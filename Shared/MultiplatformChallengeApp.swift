35203dfa0571da1bb2fff06947b67f4e2039eb7d
Rudigus <rodrigo.matos9@hotmail.com>
2021-05-18 11:09:36 -0300

    sets up xcode project, folder organization and swiftlint

---
+++ Shared/MultiplatformChallengeApp.swift
@@ -0,0 +1,20 @@
+//
+//  MultiplatformChallengeApp.swift
+//  Shared
+//
+//  Created by Rodrigo Matos Aguiar on 18/05/21.
+//
+
+import SwiftUI
+
+@main
+struct MultiplatformChallengeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
