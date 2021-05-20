5a1c61fd40abd4b25039819c5f9d0c323596be32
SamEngComp <salessamuel268@gmail.com>
2021-05-20 16:44:52 -0300

    Modal Adding Item

---
+++ Shared/View/Modal/AddItemModalView.swift
@@ -0,0 +1,55 @@
+import SwiftUI
+import Combine
+import UIKit
+
+public struct AddItemModalView: View {
+    
+    @Binding var isShowing: Bool
+    @State var nameItem: String = ""
+    @State var quantityItem: Int = 0
+    @State var priceItem: Double = 0.0
+    
+    var bodyContet: some View {
+        VStack {
+            TextFieldModalView(nameText: $nameItem,
+                               title: "Nome",
+                               placeholder: "EX.: Arroz branco")
+                .frame(height: 50)
+            CurrencyTextFieldModalView(title: "Preço",
+                                       valueFinal: $priceItem)
+                .frame(height: 50)
+            QuantityModalView(quantity: $quantityItem,
+                              title: "Quantidade",
+                              backgroundRectangleColor: Color( "ActionColorSecond"))
+                .frame(height: 50)
+            
+            //testar os State's
+//            Text(nameItem)
+//            Text("\(quantityItem)")
+//            Text("\(priceItem)")
+        }
+        .padding()
+    }
+    
+    public var body: some View {
+        VStack {
+            Spacer()
+            ModalView(isShowing: $isShowing,
+                            keyboardRef: KeyboardResponder(),
+                            titleModal: "Adicionar Produto",
+                            titleButtonLeft: "Cancelar",
+                            titleButtonRight: "Adicionar",
+                            contentBuilder: {bodyContet})
+                .onAppear(perform: UIApplication.shared.addTapGestureRecognizer)
+        }
+    }
+}
+
+struct AddItemModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            Spacer()
+            AddItemModalView(isShowing: .constant(true))
+        }
+    }
+}
