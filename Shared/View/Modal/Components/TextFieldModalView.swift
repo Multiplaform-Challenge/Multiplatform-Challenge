f1d5077e7ccaa23cafc917ab0dbe3b9bc6a9ebb9
SamEngComp <salessamuel268@gmail.com>
2021-05-20 16:44:30 -0300

    Ajust

--- Shared/View/Modal/Components/TextFieldModalView.swift
+++ Shared/View/Modal/Components/TextFieldModalView.swift
@@ -7,16 +7,19 @@ public struct TextFieldModalView: View {
     let titleFont: Font
     let foregrounColor: Color
     
-    @State var nameProduct: String = ""
+    @Binding var nameText: String
     
-    public init(title: String,
+    public init(nameText: Binding<String>,
+                title: String,
                 placeholder: String,
                 titleFont: Font = Font.headline,
                 foregrounColor: Color = Color.primary) {
+        _nameText = nameText
         self.title = title
         self.placeholder = placeholder
         self.titleFont = titleFont
         self.foregrounColor = foregrounColor
+        
     }
     
     var textFieldView: some View {
@@ -25,8 +28,9 @@ public struct TextFieldModalView: View {
                 .font(.callout)
                 .bold()
             Spacer()
-            TextField(placeholder, text: $nameProduct)
-                .frame(width: 200)
+            TextField(placeholder, text: $nameText)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .multilineTextAlignment(.trailing)
         }
         .padding(.horizontal)
     }
@@ -39,13 +43,13 @@ public struct TextFieldModalView: View {
     }
 }
 
-struct TextFieldModalView_Previews: PreviewProvider {
-
-    static var previews: some View {
-        VStack {
-            Spacer()
-            TextFieldModalView(title: "Nome", placeholder: "EX.: Arroz branco")
-            Spacer()
-        }
-    }
-}
+//struct TextFieldModalView_Previews: PreviewProvider {
+//
+//    static var previews: some View {
+//        VStack {
+//            Spacer()
+//            TextFieldModalView(title: "Nome", placeholder: "EX.: Arroz branco")
+//            Spacer()
+//        }
+//    }
+//}
