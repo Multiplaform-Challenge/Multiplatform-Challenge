d40feb08a92eca0175df208506a8a098e52b6b37
Rudigus <rodrigo.matos9@hotmail.com>
2021-05-21 10:31:10 -0300

    improves componentization of budget view cell and improves gitignore

--- Shared/View/SettingsView.swift
+++ Shared/View/SettingsView.swift
@@ -14,10 +14,7 @@ struct SettingsView: View {
     private var view: some View {
         List {
             ObjectiveViewCell(objective: $shoppingList.objective)
-            BudgetViewCell(budget: Binding(get: { self.format(value: shoppingList.budget)
-            }, set: {
-                self.shoppingList.budget = self.format(text: $0)
-            }))
+            BudgetViewCell(budget: $shoppingList.budget)
         }
     }
 
@@ -27,52 +24,10 @@ struct SettingsView: View {
             .listStyle(PlainListStyle())
     }
 
-    @State var showSeparator = false
-
-    func format(value: Double) -> String {
-        // for presentation
-        // replace "." with decimal separator
-//        print("Value: \(value)")
-        let separator = Locale.current.decimalSeparator ?? "."
-        var formattedValue = String(format: "%f", value)
-            .replacingOccurrences(of: ".", with: separator)
-            .reversed()
-            .drop(while: {$0 == "0"})
-            .reversed()
-            .map(String.init)
-            .joined()
-        if !showSeparator {
-            formattedValue.removeLast()
-        }
-        return "R$ \(formattedValue)"
-    }
-
-    let budgetCharLimit = 15
-
-    func format(text: String) -> Double {
-//        print("Text: \(text)")
-        if text.count >= budgetCharLimit {
-            return shoppingList.budget
-        }
-        let separator = Locale.current.decimalSeparator ?? "."
-        let formattedText = text
-            .replacingOccurrences(of: separator, with: ".")
-            .replacingOccurrences(of: "R", with: "")
-            .replacingOccurrences(of: "$", with: "")
-            .replacingOccurrences(of: " ", with: "")
-//        print(formattedText)
-        if formattedText.contains(".") {
-            showSeparator = true
-        } else {
-            showSeparator = false
-        }
-        return Double(formattedText) ?? 0
-    }
-
 }
 
-struct SettingsView_Previews: PreviewProvider {
-    static var previews: some View {
-        SettingsView(shoppingList: ShoppingListDemo())
-    }
-}
+//struct SettingsView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SettingsView(shoppingList: ShoppingListDemo())
+//    }
+//}
