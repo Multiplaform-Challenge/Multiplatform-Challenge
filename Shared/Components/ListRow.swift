66fb0cf871db6c59b4a3a0b7bdb57129191980e5
SamEngComp <salessamuel268@gmail.com>
2021-05-24 09:31:53 -0300

    Adjust ListRow

--- Shared/Components/ListRow.swift
+++ Shared/Components/ListRow.swift
@@ -10,40 +10,53 @@ import SwiftUI
 struct ListRow: View {
 
     let item: ProductItem
-
-    var body: some View {
-        HStack {
-            CheckboxFieldView()
-            Text(String(item.itemNumber))
-            Text(item.name)
-            Spacer()
-            Text("R$ \(String(format: "%.2f", item.price))")
-        }.padding()
-    }
-}
-
-struct CheckboxFieldView : View {
-
     @State var checkState:Bool = false
 
-    var body: some View {
+    var checkboxFieldView: some View {
          Button(action: {
                 self.checkState = !self.checkState
             }) {
-            HStack(alignment: .top, spacing: 10) {
-                   Rectangle()
-                            .fill(self.checkState ? Color.gray : Color.black)
-                            .frame(width:20, height:20, alignment: .center)
-                            .cornerRadius(10)
+            if checkState {
+                Image(systemName: "checkmark.circle.fill")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(Color("AccentColor"))
+            } else {
+                Image(systemName: "circle.fill")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .shadow(radius: 2)
             }
         }
         .foregroundColor(Color.white)
         .buttonStyle(PlainButtonStyle())
     }
+
+    var body: some View {
+        HStack {
+            checkboxFieldView
+                .frame(width: 50, height: 50)
+            VStack(alignment: .leading) {
+                Text(item.name)
+                    .font(FontNameManager.CustomFont.titleComponentFont)
+                Text("\(item.itemNumber) unidades")
+                    .font(Font.custom(FontNameManager.Poppins.regular, size: 17))
+                    .foregroundColor(Color("TextColor"))
+            }
+            Spacer()
+            VStack {
+                Text("R$ \(String(format: "%.2f", item.price))")
+                    .font(FontNameManager.CustomFont.titleComponentFont)
+            }
+        }
+        .padding()
+        .background(RoundedRectangle(cornerRadius: 15).foregroundColor(self.checkState ? Color("ActionColorSecond") : Color.white).shadow(radius: checkState ? 0 : 1))
+
+    }
 }
 
 struct ListRow_Previews: PreviewProvider {
     static var previews: some View {
-        ListRow(item: ProductItem(id: UUID.init(), name: "arroz", price: 32.99, itemNumber: 4))
+        ListRow(item: ProductItem(id: UUID.init(), name: "Arroz branco", price: 32.99, itemNumber: 4))
     }
 }
